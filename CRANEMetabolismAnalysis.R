7127dd53f6b848e6d77818f406bd197a4849ec7f
njsilbiger <nyssa.silbiger@gmail.com>
2016-05-11 18:55:40 -0700

    updated code

--- CRANEMetabolismAnalysis.R
+++ CRANEMetabolismAnalysis.R
@@ -114,14 +114,19 @@ ResTime.mean <- ddply(ChemData, c("Aquarium"), summarize,
                            )
 
 
-##NEED TO FILL IN ALL RESIDENCE TIMES WITH AN AVERAGE
-
 ## Sum up all the biological data by aquarium
 Coral.Exp1Summary <- ddply(Coral, c("Aq_Ex1"), summarize,
                           SA = sum(SA, na.rm = T),
                           AFDW = sum(AFDW, na.rm = T),
                           DW = sum(DW, na.rm = T),
                           Volume = sum(Volume, na.rm = T)
+)
+
+Coral.Exp2Summary <- ddply(Coral, c("Aq_Ex2"), summarize,
+                           SA = sum(SA, na.rm = T),
+                           AFDW = sum(AFDW, na.rm = T),
+                           DW = sum(DW, na.rm = T),
+                           Volume = sum(Volume, na.rm = T)
 )
 
 
@@ -132,6 +137,12 @@ Rubble.Exp1Summary <- ddply(Rubble, c("Aq_Ex1"), summarize,
                            Volume = sum(Volume, na.rm = T)
 )
 
+Rubble.Exp2Summary <- ddply(Rubble, c("Aq_Ex2"), summarize,
+                            SA = NA,
+                            AFDW = sum(AFDW, na.rm = T),
+                            DW = sum(DW, na.rm = T),
+                            Volume = sum(Volume, na.rm = T)
+)
 
 Algae.Exp1Summary <- ddply(Algae, c("Aq_Ex1"), summarize,
                             SA = sum(FinalSA, na.rm = T),
@@ -140,16 +151,43 @@ Algae.Exp1Summary <- ddply(Algae, c("Aq_Ex1"), summarize,
                             Volume = sum(FinalVol, na.rm = T)
 )
 
+Algae.Exp2Summary <- ddply(Algae, c("Aq_Ex2"), summarize,
+                           SA = sum(FinalSA, na.rm = T),
+                           AFDW = sum(AFDW, na.rm = T),
+                           DW = sum(DW, na.rm = T),
+                           Volume = sum(FinalVol, na.rm = T)
+)
+
 Sand.Exp1Summary <- ddply(Sand, c("Aq_Ex1"), summarize,
                            SA = sum(SA, na.rm = T),
                            AFDW = NA,
                            DW = sum(DW, na.rm = T),
                            Volume = sum(Vol, na.rm = T)
 )
+
+Sand.Exp2Summary <- ddply(Sand, c("Aq_Ex2"), summarize,
+                          SA = sum(SA, na.rm = T),
+                          AFDW = NA,
+                          DW = sum(DW, na.rm = T),
+                          Volume = sum(Vol, na.rm = T)
+)
 #join all the biology together
 biology<-rbind(Rubble.Exp1Summary,Coral.Exp1Summary, Algae.Exp1Summary,Sand.Exp1Summary)
 
+biology2<-rbind(Rubble.Exp2Summary,Coral.Exp2Summary, Algae.Exp2Summary,Sand.Exp2Summary)
+
+Exp2biology <- ddply(biology2, c("Aq_Ex2"), summarize,
+                            SA = sum(SA, na.rm = T),
+                            AFDW = sum(AFDW, na.rm = T),
+                            DW = sum(DW, na.rm = T),
+                            Volume = sum(Volume, na.rm = T)
+)
+
 colnames(biology)[1]<-'Aquarium'
+colnames(Exp2biology)[1]<-'Aquarium'
+
+biology<-rbind(biology,Exp2biology)
+
 #add the mean residence times
 AllData<-merge(ChemData, ResTime.mean, all.x=TRUE)
 
@@ -181,13 +219,14 @@ points(AllData$DateTime[AllData$NutLevel=='Med'& AllData$Substrate==sub[i]],AllD
 
 par(mfrow=c(2,2))
 for (i in 1:5){
-  plot(NEC.mean$DateTime[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]], main=sub[i])
+  plot(NEC.mean$DateTime[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]], main=sub[i], type = 'b',
+       ylim=c(min(NEC.mean$Mean[NEC.mean$Substrate==sub[i]])-0.2,max(NEC.mean$Mean[NEC.mean$Substrate==sub[i]])+0.2))
   error.bar(NEC.mean$DateTime[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]],
            NEC.mean$SE[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]],NEC.mean$SE[NEC.mean$NutLevel=='Ambient'& NEC.mean$Substrate==sub[i]])
-  points(NEC.mean$DateTime[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]], col='red')
+  lines(NEC.mean$DateTime[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]], col='red', type = 'b')
   error.bar(NEC.mean$DateTime[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]],
             NEC.mean$SE[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]],NEC.mean$SE[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]])
-  points(NEC.mean$DateTime[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]], col='blue')
+  lines(NEC.mean$DateTime[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='Med'& NEC.mean$Substrate==sub[i]], col='blue', type = 'b')
   error.bar(NEC.mean$DateTime[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]],NEC.mean$Mean[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]],
             NEC.mean$SE[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]],NEC.mean$SE[NEC.mean$NutLevel=='High'& NEC.mean$Substrate==sub[i]])
   
@@ -217,3 +256,117 @@ NEC.int[i]<-trapz(Exp1Data$DateTime[Exp1Data$Aquarium==i],Exp1Data$NECExp1[Exp1D
 ddply(Exp1Data, c("Aquarium"), summarize,
       NEC.int=trapz(Exp1Data$DateTime,Exp1Data$NECExp1)
       )
+
+##TA vs DIC plots
+b<-matrix(nrow=5,ncol=3)
+b0<-matrix(nrow=5,ncol=3)
+r2<-matrix(nrow=5,ncol=3)
+substrate<-matrix(nrow=5,ncol=3)
+Nut<-matrix(nrow=5,ncol=3)
+Nuts<-unique(AllData$NutLevel)
+colors<-c('white','blue','magenta')
+
+##make a contour plot for Deffey diagram
+AT <- seq(2000e-6, 2341e-6, length.out=10)
+DIC <- seq(1492e-6, 2300e-6, length.out=10)
+dat <- expand.grid(AT, DIC)
+
+carb <- carb(flag=15, var1=dat$Var1, var2=dat$Var2, S=35, T=25, P=0, Pt=0, Sit=0, k1k2="l", kf="pf", pHscale="T")
+arag <- carb$OmegaAragonite
+dim(arag) <- c(length(DIC), length(AT)) # 
+#contour(DIC, AT, arag)
+
+par(mfrow=c(1,1))
+for(i in 1:5){
+  
+  #plot(NA, ylim=c(2100,2200),xaxt="n", xlab="DIC", ylab="TA", main = sub[i])
+  #par(new = TRUE)
+  filled.contour(DIC*1e6, AT*1e6, arag, 
+                 xlab="DIC",
+                 ylab="Total alkalinity",
+                 key.title = title(main = expression(paste(Omega[arg]))),
+                 levels=seq(0, 9, by=0.25),  
+                 #labcex=1.5,
+                 #method="edge",
+                 col = rainbow(40),
+                 lwd=2,
+                 lty="solid",
+                 main=sub[i]
+                    
+  )
+   #calcification dissolution line
+  x.DIC<-seq(from=1820, to= 1950, by  = 1)
+  y.TA<-2*x.DIC-1600
+  lines(x.DIC,y.TA, col='black', lwd=3)
+  arrows(min(x.DIC),min(y.TA),max(x.DIC), max(y.TA), col='black', code=3,lwd=3)
+  text(max(x.DIC)-15, max(y.TA)+20, 'Calcification')
+  text(min(x.DIC)-15, min(y.TA)-20, 'Dissolution')
+  #photosynthesis/respiration line
+  x.DIC<-seq(from=1720, to= 2100, by  = 1)
+  y.TA<-0.16*x.DIC +1850
+  lines(x.DIC,y.TA, col='black', lwd=3)
+  arrows(min(x.DIC),min(y.TA),max(x.DIC), max(y.TA), col='black', code=3,lwd=3)
+  text(max(x.DIC)-15, max(y.TA)+20, 'Photosynthesis')
+  text(min(x.DIC)-15, min(y.TA)-20, 'Respiration')
+    #abline(h=0)
+   # par(new = TRUE)
+    #plot(AllData$TankDIC[ AllData$Substrate==sub[i]],AllData$TankTA[ AllData$Substrate==sub[i]], main=sub[i], 
+     #     type = 'p', col = colors[j], pch=19, yaxt="n", ylab="", xlab="")
+    for (j in 1:3){
+      #par(new = TRUE)
+    points(AllData$TankDIC[AllData$NutLevel==Nuts[j] & AllData$Substrate==sub[i]],AllData$TankTA[AllData$NutLevel==Nuts[j]& AllData$Substrate==sub[i]], main=sub[i], 
+           type = 'p', col = colors[j], pch=19)
+    
+    
+    modelTA.DIC<-lm(AllData$TankTA[AllData$NutLevel==Nuts[j]& AllData$Substrate==sub[i]]  ~AllData$TankDIC[AllData$NutLevel==Nuts[j] & AllData$Substrate==sub[i]])
+    b0[i,j]<-modelTA.DIC$coefficients[1]
+    b[i,j]<-modelTA.DIC$coefficients[2]
+    r2[i,j]<-summary(modelTA.DIC)$r.squared
+    substrate[i,j]<-sub[i]
+    Nut[i,j]<-Nuts[j]
+    #x<-seq(from=1500, to=2100,by=0.5)
+    y<-b[i,j]*x+b0[i,j]
+    lines(x,y, col=colors[j], lwd=3)
+    
+    
+    
+    }
+legend('topleft',legend=c('Ambient',"Medium","High"), col=c('blue','magenta','white'), pch=19, bty = 'n')
+}
+
+
+##make a contour plot
+AT <- seq(2080e-6, 2241e-6, length.out=10)
+DIC <- seq(1592e-6, 2068e-6, length.out=10)
+dat <- expand.grid(AT, DIC)
+
+carb <- carb(flag=15, var1=dat$Var1, var2=dat$Var2, S=35, T=25, P=0, Pt=0, Sit=0, k1k2="l", kf="pf", pHscale="T")
+
+
+# Do a simple contour plot
+arag <- carb$OmegaAragonite
+dim(arag) <- c(length(DIC), length(AT)) # 
+contour(DIC, AT, arag)
+
+
+filled.contour(DIC*1e6, AT*1e6, arag, 
+        xlab="DIC",
+        ylab="Total alkalinity (umol/kg)",
+        levels=seq(1, 7, by=0.25),  
+        labcex=1.5,
+        method="edge",
+        col = rainbow(40),
+        lwd=2,
+        lty="solid"
+)
+par(new = TRUE)
+image(DIC*1e6, AT*1e6, arag, col=rainbow(6),useRaster=TRUE,
+      xlab = "X Coordinate (feet)", ylab = "Y Coordinate (feet)",
+      main = "Surface elevation data")
+box()
+
+levelplot(arag ~ DIC*1e6 + AT*1e6, 
+          xlab = "X Coordinate (feet)", ylab = "Y Coordinate (feet)",
+          main = "Surface elevation data",
+          col.regions = terrain.colors(100)
+)
