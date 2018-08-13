e05505c030cb0d4df8eee3f8ebe83d1e9bc50252
njsilbiger <nsilbige@uci.edu>
2018-08-13 08:25:18 -0700

    added light normalization

--- Scaling/NutrientUptakeAnalysis.R
+++ Scaling/NutrientUptakeAnalysis.R
@@ -85,23 +85,165 @@ colnames(DataEx1)[1] <- "Aq_Ex1"
 # merge with the NEC and NEP data
 ScalDat<-left_join(DataEx1,ScalDat,by=c("Aq_Ex1","Substrate"))
 
+#### light normalization #####
+## run the light script
+source('Scaling/lightnormalization.R')
+
+## average NCP and NEC rates by substrate and incubation tank
+TankRates<-AllData %>%
+  group_by(Substrate,Tank, NutLevel) %>%
+  summarise_at(c('NCP.AFDW','NEC.AFDW'),  mean)
+
+# create an empty dataframe to put the coefficients in 
+coefs.sub.nut<-data.frame(matrix(NA, nrow=12, ncol=6))
+colnames(coefs.sub.nut)<-c('Substrate','NutLevel','NCP.Intercept','NCP.Slope','NEC.Intercept','NEC.Slope')                          
+coefs.sub.nut$Substrate<-rep(sub[1:4],3)
+coefs.sub.nut$NutLevel<-c(rep(as.character(Nuts[1]),4),
+                          rep(as.character(Nuts[2]),4),rep(as.character(Nuts[3]),4))
+# Light by NCP plot across substrates and nutrients
+png('Scaling/plots/LightNormNCP.png',width = 10000, height = 12000, res = 800 )
+par(mfrow=c(4,3))
+for (i in 1:4){
+  for (j in 1:3){
+  plot(CumLight[1,], TankRates$NCP.AFDW[TankRates$Substrate==sub[i] & TankRates$NutLevel == Nuts[j]], 
+                  cex.lab = 2,cex.main = 2, cex.axis = 1.5, cex = 1.5,main = paste(sub[i], Nuts[j]), pch = 19,xlab = 'Cumulative PAR', ylab = 'mean NCP')
+mod<-lm(TankRates$NCP.AFDW[TankRates$Substrate==sub[i] & TankRates$NutLevel == Nuts[j]]~CumLight[1,])
+lines(CumLight[1,],predict(mod))
+legend('topleft',c(paste('r2 = ',round(summary(mod)$r.squared,2)),paste('slope = ',formatC(coef(mod)[2], format = "e", digits = 2))), bty = 'n', cex = 2)
+coefs.sub.nut[coefs.sub.nut$Substrate==sub[i] & coefs.sub.nut$NutLevel == Nuts[j],3:4]<-coef(mod)
+      }
+  }
+dev.off()
+
+## same figures, but only across substrates
+## average NCP and NEC rates by substrate and incubation tank
+TankRates.sub<-AllData %>%
+  group_by(Substrate,Tank) %>%
+  summarise_at(c('NCP.AFDW','NEC.AFDW'),  mean)
+#NCP
+png('Scaling/plots/LightNormNCPSub.png',width = 10000, height = 12000, res = 800 )
+par(mfrow=c(2,2))
+for (i in 1:4){
+    plot(CumLight[1,], TankRates.sub$NCP.AFDW[TankRates.sub$Substrate==sub[i]], 
+         cex.lab = 2,cex.main = 2, cex.axis = 1.5, cex = 1.5,main = paste(sub[i]), pch = 19,xlab = 'Cumulative PAR', ylab = 'mean NCP')
+    mod<-lm(TankRates.sub$NCP.AFDW[TankRates.sub$Substrate==sub[i]]~CumLight[1,])
+    lines(CumLight[1,],predict(mod))
+    legend('topleft',c(paste('r2 = ',round(summary(mod)$r.squared,2)),paste('slope = ',formatC(coef(mod)[2], format = "e", digits = 2))), bty = 'n', cex = 2)
+     }
+
+dev.off()
+#NEC
+png('Scaling/plots/LightNormNECSub.png',width = 10000, height = 12000, res = 800 )
+par(mfrow=c(2,2))
+for (i in 1:4){
+  plot(CumLight[1,], TankRates.sub$NEC.AFDW[TankRates.sub$Substrate==sub[i]], 
+       cex.lab = 2,cex.main = 2, cex.axis = 1.5, cex = 1.5,main = paste(sub[i]), pch = 19,xlab = 'Cumulative PAR', ylab = 'mean NCC')
+  mod<-lm(TankRates.sub$NEC.AFDW[TankRates.sub$Substrate==sub[i]]~CumLight[1,])
+  lines(CumLight[1,],predict(mod))
+  legend('topleft',c(paste('r2 = ',round(summary(mod)$r.squared,2)),paste('slope = ',formatC(coef(mod)[2], format = "e", digits = 2))), bty = 'n', cex = 2)
+}
+
+dev.off()
+# Light by NEC plot across substrates and nutrients
+png('Scaling/plots/LightNormNEC.png',width = 10000, height = 12000, res = 800 )
+par(mfrow=c(4,3))
+for (i in 1:4){
+  for (j in 1:3){
+    plot(CumLight[1,], TankRates$NEC.AFDW[TankRates$Substrate==sub[i] & TankRates$NutLevel == Nuts[j]], 
+         cex.lab = 2,cex.main = 2, cex.axis = 1.5, cex = 1.5,main = paste(sub[i], Nuts[j]), pch = 19,xlab = 'Cumulative PAR', ylab = 'mean NCC')
+    mod<-lm(TankRates$NEC.AFDW[TankRates$Substrate==sub[i] & TankRates$NutLevel == Nuts[j]]~CumLight[1,])
+    lines(CumLight[1,],predict(mod))
+    legend('topleft',c(paste('r2 = ',round(summary(mod)$r.squared,2)),paste('slope = ',formatC(coef(mod)[2], format = "e", digits = 2))), bty = 'n', cex = 2)
+    coefs.sub.nut[coefs.sub.nut$Substrate==sub[i] & coefs.sub.nut$NutLevel == Nuts[j],5:6]<-coef(mod)
+    
+      }
+}
+dev.off()
+
 # calculate estimated rate based on proportion of weight
 ScalDat$NN.wtd<-ScalDat$N.uptake*ScalDat$propAFDW
 ScalDat$PO.wtd<-ScalDat$P.uptake*ScalDat$propAFDW
 ScalDat$NCP.wtd<-ScalDat$NCP.AFDW*ScalDat$propAFDW
 ScalDat$NEC.wtd<-ScalDat$NEC.AFDW*ScalDat$propAFDW
 
-
+### normalize the NEC and NCP data to constant light. ##
+# Take the relationship between cumulative light and the rate (NCP, NEC). Put in the delta light 
+# between day 1 and 2 into the equation for each bin x substrate. The NCP value calculated from this 
+# equation is how much the NCP changed between day 1 and 2 due to differences in light. This value gets
+#subtracted from the predicted NCP to account for light effects. THis need to be done on the average daily NCP and NEC
+# based on how I ran the regressions
+# delta cumulative light between day 1 ans 2
+deltaLight<-data.frame(as.matrix(CumLight[1,] - CumLight[2,]))
+deltaLight$Tank<-c(1,2,3)
+colnames(deltaLight)[1]<-'deltaPAR'
 # now calculate the predicted
 DataEx2 <- AllData[AllData$Experiment==2,]
 colnames(DataEx2)[1] <- "Aq_Ex2"
 colnames(DataEx2)[49] <- "DateTimeEx2"
 
+#take the daily averages 
+ScalDat_Ave<-ScalDat %>%
+  group_by(Aq_Ex2, NutLevel, Substrate, Tank, Experiment) %>%
+  summarise(NN.pred = mean(NN.wtd), PO.pred = mean(PO.wtd), NCP.pred = mean(NCP.wtd), NEC.pred = mean(NEC.wtd))%>%
+  left_join(.,deltaLight)
+ScalDat_Ave<-ScalDat_Ave[-c(121:nrow(ScalDat_Ave)),]
+
+# add light coefs 
+ScalDat_Ave<-left_join(ScalDat_Ave, coefs.sub.nut)
+#scale to light
+ScalDat_Ave$NCP.pred.light<-with(ScalDat_Ave,NCP.pred+(NCP.Intercept+deltaPAR*NCP.Slope))
+ScalDat_Ave$NEC.pred.light<-with(ScalDat_Ave,NEC.pred+(NEC.Intercept+deltaPAR*NEC.Slope))
+
+# calculate observed - predicted
+# sum across all 4 substrates in each aquarium
+ScalDat_Avesum<-ScalDat_Ave %>%
+  group_by(Aq_Ex2, NutLevel) %>%
+  summarise(NCP.pred = sum(NCP.pred.light), NEC.pred = sum(NEC.pred.light))
+
+# average the observed data
+DataEx2_Ave<-DataEx2 %>% group_by(Aq_Ex2, NutLevel, Tank) %>%
+  summarise(NCP.mean = mean(NCP.AFDW), NEC.mean = mean(NEC.AFDW))
+
+# bring together the observed and predicted
+DataEx2_Ave<-left_join(ScalDat_Avesum, DataEx2_Ave)
+# calculate observed - predicted
+DataEx2_Ave <- mutate(DataEx2_Ave,
+                  NCP.diff = NCP.mean - NCP.pred,
+                  NEC.diff = NEC.mean - NEC.pred)
+
+# does nutrients affect scaling?
+mod1.NEC.diff <- lmer(NEC.diff ~ NutLevel
+                     + (1|Tank),data=DataEx2_Ave)
+mod1.NCP.diff <- lmer(NCP.diff ~ NutLevel
+                      + (1|Tank),data=DataEx2_Ave)
+
+# plot
+Uptake.diff.means<-DataEx2_Ave %>%
+  group_by(NutLevel) %>%
+  summarise(.,NCP.diff.mean = mean(NCP.diff), NCP.diff.SE = sd(NCP.diff)/sqrt(n()),NEC.diff.mean = mean(NEC.diff), NEC.diff.SE = sd(NEC.diff)/sqrt(n()) )
+
+## NCP
+par(mfrow= c(1,2))
+plot(1:3, Uptake.diff.means$NCP.diff.mean, 
+     pch = 19, cex = 2, ylim = c(-10,10), xaxt = 'n', ylab = 'Difference in rate', xlab = '', main = 'NCP')
+segments(1:3, Uptake.diff.means$NCP.diff.mean+Uptake.diff.means$NCP.diff.SE, 
+         1:3, Uptake.diff.means$NCP.diff.mean-Uptake.diff.means$NCP.diff.SE)
+abline(h=0)
+#NEC
+axis(1, at = c(1:3), c("Ambient","Medium","High"))
+plot(1:3, Uptake.diff.means$NEC.diff.mean, pch = 19, cex = 2, ylim = c(-10,10), xaxt = 'n', xlab = '', main = 'NEC')
+segments(1:3, Uptake.diff.means$NEC.diff.mean+Uptake.diff.means$NEC.diff.SE, 
+         1:3, Uptake.diff.means$NEC.diff.mean-Uptake.diff.means$NEC.diff.SE)
+abline(h=0)
+axis(1, at = c(1:3), c("Ambient","Medium","High"))
+
+########################### stop ################################
+#### without scaling to light or averaging over the day
 ScalDat_Aqua<-ScalDat %>%
   group_by(Aq_Ex2, DateTime,NutLevel) %>%
   summarise(NN.pred = sum(NN.wtd), PO.pred = sum(PO.wtd), NCP.pred = sum(NCP.wtd), NEC.pred = sum(NEC.wtd))
 
-ScalDat_Aqua$DateTimeEx2<-ScalDat_Aqua$DateTime+60*60*48
+ScalDat_Aqua$DateTimeEx2<-ScalDat_Aqua$DateTime+60*60*48 # get the correct time
 
 DataEx2 <- left_join(DataEx2,ScalDat_Aqua,by=c("Aq_Ex2","DateTimeEx2","NutLevel"))
 levels(DataEx2$DateTimeEx2)<- c(1:7)
---
+++ Scaling/lightnormalization.R
@@ -0,0 +1,73 @@
+### light normalization calculation
+## created on 8/11/2018
+# created by Nyssa Silbiger
+
+#clear the workspace
+#rm(list = ls())
+
+# load libraries
+library(tidyverse)
+library(pROC)
+
+#Read in the light from Tank A
+LightA<-read.csv('data/light/20151224_ODY4806_A.CSV')
+LightA$Date<-as.Date(LightA$Date, "%d/%m/%Y")
+LightA$DateTime<-as.POSIXct(paste(LightA$Date, LightA$Time), format="%Y-%m-%d %H:%M:%S")
+LightA[c('ï..Scan.No','CALIBRATED.VALUE')]<-NULL
+colnames(LightA)[3]<-'PAR.A'
+LightA$PAR.A<-0.0613*LightA$PAR.A^0.9414 # calibrations to LiCor from Hollie Putnam 
+#Tank B
+LightB<-read.csv('data/light/20151224_ODY4802_B.CSV')
+LightB$Date<-as.Date(LightB$Date, "%d/%m/%Y")
+LightB$DateTime<-as.POSIXct(paste(LightB$Date, LightB$Time), format="%Y-%m-%d %H:%M:%S")
+LightB[c('ï..Scan.No','Calibrated')]<-NULL
+colnames(LightB)[3]<-'PAR.B'
+LightB$PAR.B<-0.0682*LightB$PAR.B^0.9334 # calibrations to LiCor from Hollie Putnam 
+
+#Tank C
+LightC<-read.csv('data/light/20151224_ODY2483_C.CSV')
+LightC$Date<-as.Date(LightC$Date, "%d/%m/%Y")
+LightC$DateTime<-as.POSIXct(paste(LightC$Date, LightC$Time), format="%Y-%m-%d %H:%M:%S")
+LightC[c('ï..Scan.No','CALIBRATED.VALUE')]<-NULL
+colnames(LightC)[3]<-'PAR.C'
+LightC$PAR.C<-0.0876*LightC$PAR.C^0.9316 # calibrations to LiCor from Hollie Putnam 
+
+## join all the light data into one dataframe by datetime
+AllLight<-left_join(LightA, LightB)
+AllLight<-left_join(AllLight,LightC)
+#re-order the columns
+AllLight<-AllLight[c(1,2,4,3,5,6)]
+
+# plot all the light data
+#plot(AllLight$DateTime, AllLight$PAR.A, type = 'l', col = 'red')
+#lines(AllLight$DateTime, AllLight$PAR.B, type = 'l', col = 'blue')
+#lines(AllLight$DateTime, AllLight$PAR.C, type = 'l', col = 'green')
+
+# extract data from experiment 1
+Exp1<-which(AllLight$DateTime>=as.POSIXct('2015-12-03 10:00:00', format="%Y-%m-%d %H:%M:%S") &
+        AllLight$DateTime <= as.POSIXct('2015-12-04 10:00:00', format="%Y-%m-%d %H:%M:%S"))
+LightExp1<-AllLight[Exp1,] # new light df
+
+# extract light from exp 2
+Exp2<-which(AllLight$DateTime>=as.POSIXct('2015-12-05 10:00:00', format="%Y-%m-%d %H:%M:%S") &
+              AllLight$DateTime <= as.POSIXct('2015-12-06 10:00:00', format="%Y-%m-%d %H:%M:%S"))
+LightExp2<-AllLight[Exp2,] # new light df
+
+# plot day 1 ves 2 in each bin
+par(mfrow=c(2,2))
+plot(LightExp1$DateTime, LightExp1$PAR.A, type = 'l', col = 'red', ylab = 'PAR', xlab = 'Time', main = 'Tank A')
+lines(LightExp1$DateTime, LightExp2$PAR.A, type = 'l', col = 'blue')
+legend('topright',c('Exp 1', 'Exp 2'), lty = 1, col = c('red','blue'), bty='n')
+plot(LightExp1$DateTime, LightExp1$PAR.B, type = 'l', col = 'red', ylab = 'PAR', xlab = 'Time', main = 'Tank B')
+lines(LightExp1$DateTime, LightExp2$PAR.B, type = 'l', col = 'blue')
+plot(LightExp1$DateTime, LightExp1$PAR.C,type = 'l', col = 'red', ylab = 'PAR', xlab = 'Time', main = 'Tank C')
+lines(LightExp1$DateTime, LightExp2$PAR.C, type = 'l', col = 'blue')
+
+# cumulative light for each experiment
+Exp1.sum<-colSums(LightExp1[,c("PAR.A","PAR.B","PAR.C")])
+Exp2.sum<-colSums(LightExp2[,c("PAR.A","PAR.B","PAR.C")])
+CumLight<-rbind(Exp1.sum,Exp2.sum)
+
+barplot(CumLight, beside = T, col = c('red','blue'), names.arg = c('A','B','C'), ylab = 'Cumulative PAR')
+
+        
